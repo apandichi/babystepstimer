bdd38354e365656d5a2c40e9cd678c0c804bca16
Alin Pandichi <alin.pandichi@gmail.com>
2017-01-23 17:14:10 +0200

    Refactoring SoundPlayerImpl

--- Java/src/main/java/net/davidtanzer/babysteps/SoundPlayerImpl.java
+++ Java/src/main/java/net/davidtanzer/babysteps/SoundPlayerImpl.java
@@ -1,18 +1,17 @@
 package net.davidtanzer.babysteps;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioSystem.getClip;
 
 public class SoundPlayerImpl implements SoundPlayer {
     @Override
     public synchronized void playSound(String url) {
         new Thread(() -> {
             try {
-                Clip clip = AudioSystem.getClip();
-                AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                        this.getClass().getResourceAsStream("/"+url));
-                clip.open(inputStream);
+                Clip clip = getClip();
+                clip.open(getAudioInputStream(this.getClass().getResourceAsStream("/" + url)));
                 clip.start();
             } catch (Exception e) {
                 System.err.println(e.getMessage());
