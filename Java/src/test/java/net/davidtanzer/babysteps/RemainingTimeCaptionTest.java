c2bcf3702a3afbbe4e7b3f546f1fe8a1300f7bee
Alin Pandichi <alin.pandichi@gmail.com>
2017-01-23 16:37:59 +0200

    Remove dependency on BabystepsTimer constant from RemainingTimeCaptionTest

--- Java/src/test/java/net/davidtanzer/babysteps/RemainingTimeCaptionTest.java
+++ Java/src/test/java/net/davidtanzer/babysteps/RemainingTimeCaptionTest.java
@@ -8,25 +8,29 @@ public class RemainingTimeCaptionTest {
 
     @Test
     public void when0MillisecondsElapsedCaptionShouldBe20Seconds() {
-        String caption = remainingTimeCaption.getRemainingTimeCaption(0L, BabystepsTimer.SECONDS_IN_CYCLE);
+        long secondsInCycle = 20L;
+        String caption = remainingTimeCaption.getRemainingTimeCaption(0L, secondsInCycle);
         Assert.assertEquals(caption, "00:20");
     }
 
     @Test
     public void when10MillisecondsElapsedCaptionShouldBe20Seconds() {
-        String caption = remainingTimeCaption.getRemainingTimeCaption(10L, BabystepsTimer.SECONDS_IN_CYCLE);
+        long secondsInCycle = 20L;
+        String caption = remainingTimeCaption.getRemainingTimeCaption(10L, secondsInCycle);
         Assert.assertEquals(caption, "00:20");
     }
 
     @Test
     public void when1000MillisecondsElapsedCaptionShouldBe19Seconds() {
-        String caption = remainingTimeCaption.getRemainingTimeCaption(1000L, BabystepsTimer.SECONDS_IN_CYCLE);
+        long secondsInCycle = 20L;
+        String caption = remainingTimeCaption.getRemainingTimeCaption(1000L, secondsInCycle);
         Assert.assertEquals(caption, "00:19");
     }
 
     @Test
     public void when2000MillisecondsElapsedCaptionShouldBe18Seconds() {
-        String caption = remainingTimeCaption.getRemainingTimeCaption(2000L, BabystepsTimer.SECONDS_IN_CYCLE);
+        long secondsInCycle = 20L;
+        String caption = remainingTimeCaption.getRemainingTimeCaption(2000L, secondsInCycle);
         Assert.assertEquals(caption, "00:18");
     }
 }
