9fce761084f6a5a91e504f07dc062c9a1ca1e36f
Alin Pandichi <alin.pandichi@gmail.com>
2017-01-23 17:33:32 +0200

    Add tests with Mockito for sound player

---
+++ Java/src/test/java/net/davidtanzer/babysteps/SoundPlayerTest.java
@@ -0,0 +1,43 @@
+package net.davidtanzer.babysteps;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class SoundPlayerTest {
+    private SoundPlayer soundPlayer = new SoundPlayerImpl();
+
+    @Mock
+    ErrorPrinter errorPrinter;
+
+    @Test
+    public void tryingToPlayInexistentSoundFileWillPrintNull() {
+        MockitoAnnotations.initMocks(this);
+        soundPlayer.tryToPlaySound("inexistent file", errorPrinter);
+        verify(errorPrinter).printError(null);
+    }
+
+    @Test
+    public void tryingToPlayInexistentThetoneSoundFileWillPrintNull() {
+        MockitoAnnotations.initMocks(this);
+        soundPlayer.tryToPlaySound("thetone.wav", errorPrinter);
+        verify(errorPrinter).printError(null);
+    }
+
+    @Test
+    public void tryingToPlayPluckSound() {
+        MockitoAnnotations.initMocks(this);
+        soundPlayer.tryToPlaySound("pluck.wav", errorPrinter);
+        verify(errorPrinter, never()).printError(null);
+    }
+
+    @Test
+    public void tryingToPlayTheetoneSound() {
+        MockitoAnnotations.initMocks(this);
+        soundPlayer.tryToPlaySound("theetone.wav", errorPrinter);
+        verify(errorPrinter, never()).printError(null);
+    }
+}
