7ef02f2d3581b659f20fab4ff1191c545a9063ac
Alin Pandichi <alin.pandichi@mozaicworks.com>
2017-01-25 20:05:03 +0200

    Test HTML renderer output

--- Java/src/main/java/net/davidtanzer/babysteps/BabystepsTimer.java
+++ Java/src/main/java/net/davidtanzer/babysteps/BabystepsTimer.java
@@ -128,31 +128,4 @@ public class BabystepsTimer {
 		}).start();
 	}
 
-	private static class HTMLRenderer {
-		private String timerText;
-		private String bodyColor;
-		private boolean running;
-
-		public HTMLRenderer(String timerText, String bodyColor, boolean running) {
-			this.timerText = timerText;
-			this.bodyColor = bodyColor;
-			this.running = running;
-		}
-
-		public String invoke() {
-			String timerHtml = "<html><body style=\"border: 3px solid #555555; background: "+bodyColor+"; margin: 0; padding: 0;\">" +
-                    "<h1 style=\"text-align: center; font-size: 30px; color: #333333;\">"+timerText+"</h1>" +
-                    "<div style=\"text-align: center\">";
-			if(running) {
-                timerHtml += "<a style=\"color: #555555;\" href=\"command://stop\">Stop</a> " +
-                        "<a style=\"color: #555555;\" href=\"command://reset\">Reset</a> ";
-            } else {
-                timerHtml += "<a style=\"color: #555555;\" href=\"command://start\">Start</a> ";
-            }
-			timerHtml += "<a style=\"color: #555555;\" href=\"command://quit\">Quit</a> ";
-			timerHtml += "</div>" +
-                    "</body></html>";
-			return timerHtml;
-		}
-	}
 }
