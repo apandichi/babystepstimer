86f5664a25ff10487183b0d30a4226375833d060
Alin Pandichi <alin.pandichi@mozaicworks.com>
2017-02-24 11:33:14 +0200

    Refactor BabystepsHyperlinkListener: replaced if else if structure with a map of functions

--- Java/src/main/java/net/davidtanzer/babysteps/BabystepsHyperlinkListener.java
+++ Java/src/main/java/net/davidtanzer/babysteps/BabystepsHyperlinkListener.java
@@ -2,26 +2,30 @@ package net.davidtanzer.babysteps;
 
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import java.util.HashMap;
+import java.util.Map;
 
 class BabystepsHyperlinkListener implements HyperlinkListener {
     private BabystepsTimerUserInterface babystepsTimer;
+    Map<String, Runnable> mapHyperlinkToFunction = new HashMap<>();
+
     public BabystepsHyperlinkListener(BabystepsTimerUserInterface babystepsTimer) {
         this.babystepsTimer = babystepsTimer;
+        initializeMapHyperlinkToFunction();
+    }
+
+    private void initializeMapHyperlinkToFunction() {
+        mapHyperlinkToFunction.put("command://start", () -> babystepsTimer.start());
+        mapHyperlinkToFunction.put("command://stop", () -> babystepsTimer.stop());
+        mapHyperlinkToFunction.put("command://reset", () -> babystepsTimer.reset());
+        mapHyperlinkToFunction.put("command://quit", () -> babystepsTimer.quit());
     }
 
     @Override
     public void hyperlinkUpdate(final HyperlinkEvent e) {
         if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-            String description = e.getDescription();
-            if("command://start".equals(description)) {
-                babystepsTimer.start();
-            } else if("command://stop".equals(description)) {
-                babystepsTimer.stop();
-            } else  if("command://reset".equals(description)) {
-                babystepsTimer.reset();
-            } else  if("command://quit".equals(description)) {
-                babystepsTimer.quit();
-            }
+            String hyperlink = e.getDescription();
+            mapHyperlinkToFunction.get(hyperlink).run();
         }
     }
 }
